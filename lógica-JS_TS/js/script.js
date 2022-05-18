5ad1b99ba00f7ea81d39254937514cc2de3e257f
João Victor Martins <63604234+JoaoVictorMartinsMedeiros@users.noreply.github.com>
2022-05-18 15:34:45 -0300

    Add files via upload

--- lógica-JS_TS/js/script.js
+++ lógica-JS_TS/js/script.js
@@ -1,17 +1,34 @@
+const calcular = document.getElementById('btn');
+const resultado = document.getElementById('resultado');
 
-
-
-
-
-function calculaIMC(){
-    var btn = document.querySelector('btn');
-    var peso =  document.querySelector('input-Peso');
-    var altura = document.querySelector('input-Altura');
+function style(){
+    resultado.style.backgroundColor = '#136864';
+    resultado.style.color = 'white';
+    resultado.style.margin = '0 155px';
+    resultado.style.width = 'fit-content';
+    resultado.style.padding = '5px 5px';
     
-    var calculoIMC = peso/sqr(altura);
-
-    console.log(calculoIMC);
-    btn.onclick = calculaIMC()
-
 }
+
+
+function imc() {
+    
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
+    
+    if (nome !== '' && peso !== '' && altura !== '') {
+        style();
+        resultado.textContent = nome + ',seu valor de IMC é ' + valorIMC + ' verifique a tabela para mais informações.';
+    } else {
+        style();
+        resultado.textContent = 'Preencha todos os campos!!!';
+
+    }
+}
+
+
+calcular.addEventListener('click', imc);
+
 
